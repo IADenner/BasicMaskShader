4b18f5ec4db94a32b711ebede102860895c59f72
Isaac Denner <iadenner@gmail.com>
2022-03-10 16:16:23 -0500

    Add files via upload

--- BasicMaskShader.hx
+++ BasicMaskShader.hx
@@ -1,4 +1,5 @@
 package shaders;
+import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxShader;
 import gameTools.things.MySprite;
 
@@ -21,7 +22,7 @@ class BasicMaskShader extends FlxShader
 	void main()
 	{
 		
-		if (texture2D(mainPalette, vec2(openfl_TextureCoordv.x * sizeX + relativePosX, openfl_TextureCoordv.y * sizeY + relativePosY)).a > 0.1)
+		if (texture2D(mainPalette, vec2(openfl_TextureCoordv.x * sizeX - relativePosX, openfl_TextureCoordv.y * sizeY - relativePosY)).a > 0.1)
 		{
 			gl_FragColor = texture2D(bitmap, openfl_TextureCoordv);
 		}
@@ -59,7 +60,7 @@ class BasicMaskShader extends FlxShader
 	public function setData(maskSprite:String, tsizeX:Float = 1, tsizeY:Float = 1, trelativePosX:Float = 0, trelativePosY:Float = 0)
 	{
 		
-		var mp:MySprite = new MySprite(0, 0);
+		var mp:FlxSprite = new FlxSprite(0, 0);
 		
 		//for some reason I couldn't get BitmapData.loadFile() to work
 		mp.loadGraphic(maskSprite);
@@ -78,8 +79,8 @@ class BasicMaskShader extends FlxShader
 		relativePosX.value = [trelativePosX];
 		relativePosY.value = [trelativePosY];
 		
-		sizeX.value = [tsizeX];
-		sizeY.value = [tsizeY];
+		sizeX.value = [1/tsizeX];
+		sizeY.value = [1/tsizeY];
 	}
 
 	
